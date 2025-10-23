a5bfc0ac87fd36717a14c27b4ea2dfaaa9a3a2df
amandx36 <xamandeep360@gmail.com>
2025-10-23 18:07:29 +0530

    Starting of project

---
+++ src/main/java/com/CollageProject/collageDiscord/CollageDiscordApplication.java
@@ -0,0 +1,13 @@
+package com.CollageProject.collageDiscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageDiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageDiscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CollageProject/collageDiscord/CollageDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CollageProject.collageDiscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollageDiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
