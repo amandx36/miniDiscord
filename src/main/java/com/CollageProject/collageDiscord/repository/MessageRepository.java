b62e4089bb07e7104ae128a969c928359a2e9c53
amandx36 <xamandeep360@gmail.com>
2025-10-23 18:43:15 +0530

    Made a interface to implement the monogodb // or use the mongodb query and made one custom query  to find all messages in ascending order in timeinterval

---
+++ src/main/java/com/CollageProject/collageDiscord/repository/MessageRepository.java
@@ -0,0 +1,20 @@
+package com.CollageProject.collageDiscord.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.CollageProject.collageDiscord.model.Message;
+
+public interface MessageRepository extends MongoRepository {
+
+    // we are making the interface so we can implement the mongo functions such as save find find all and the other things !!! 
+
+    // we are using this functions in the chatServices Bro !! 
+
+    // making our first custom made query etch all messages from MongoDB and sort them by the timestamp field in ascending order.  
+    // mongodb make query using the functions name !!!
+
+    List<Message> findAllBYOrderBYTimestampAsc();
+    
+}
